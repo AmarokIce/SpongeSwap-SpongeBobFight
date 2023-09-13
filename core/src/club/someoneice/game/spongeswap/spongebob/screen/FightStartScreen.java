00b70542c1ada93aa1503021955e01959762017b
Amarok <91077200+AmarokIce@users.noreply.github.com>
2023-09-13 23:44:43 +0800

    💻 Restart the project and init.

---
+++ core/src/club/someoneice/game/spongeswap/spongebob/screen/FightStartScreen.java
@@ -0,0 +1,10 @@
+package club.someoneice.game.spongeswap.spongebob.screen;
+
+import club.someoneice.game.spongeswap.spongebob.util.BaseScreen;
+
+public class FightStartScreen extends BaseScreen {
+    @Override
+    public void join() {
+
+    }
+}
